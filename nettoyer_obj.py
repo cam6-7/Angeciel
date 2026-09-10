9c340beec3339aa35149a6f4c0b2a90e68b8c73c
Cam6-7 <camille.messineo@free.fr>
2026-09-10 22:36:37 +0200

    réparage de nettoyer objets

--- nettoyer_obj.py
+++ nettoyer_obj.py
@@ -2,35 +2,27 @@ import os, glob, json
 
 from entities.class_Niveau import Niveau
 from entities.class_Plateforme import Plateforme
-
 dossier = os.path.dirname(os.path.abspath(__file__))
-nombre_de_niveau = len(glob.glob(dossier + "/objets/niveau*.json"))
+nombre_de_niveau = len(glob.glob(dossier + "/objets/plateforme*.json"))
 nb_bon_niv = 4
-for numero in range(nb_bon_niv + 1, nombre_de_niveau + 1):
-    os.remove(dossier + "/objets/niveau" + str(numero) + ".json")
-    os.remove(dossier + "/objets/plateforme" + str(numero) + ".json")
-print("\033[32msupression de", nombre_de_niveau - len(glob.glob(dossier + "/objets/niveau*.json")), "niveaux réussi\033[0m")
+nombre_de_suppression = 0
 
-for i in range(1, nombre_de_niveau+1):
-    with open("objets/plateforme"+str(i)+".json", "r") as f:
-        data = json.load(f)
-    for plat in data:
-        Plateforme(plat["niveau"], plat["taille"], plat["positions"])
-# niveaux
-for i in range(1, nombre_de_niveau+1):
-    with open("objets/niveau"+str(i)+".json", "r") as f:
-        data = json.load(f)
-        Niveau(
-            Plateforme.liste[i],
-            data["couleur"],
-            name = data["name"]
-        )
+for numero in range(nb_bon_niv + 1, nombre_de_niveau + 1):
+    os.remove(dossier + "/objets/plateforme" + str(numero) + ".json")
+    nombre_de_suppression += 1
+print("\033[32msupression de", nombre_de_suppression, "niveaux réussi\033[0m")
+
 
 nombre_de_suppression = 0
+nombre_de_niveau = len(glob.glob(dossier + "/objets/plateforme*.json"))
+for niv in range(1, nombre_de_niveau + 1):
+    Niveau()
+    with open("objets/plateforme" + str(niv) + ".json", "r") as f:
+        [Plateforme(plat["niveau"], plat["taille"], plat["positions"], plat["avance"]) for plat in json.load(f)]
 for niv in Niveau.liste:
     pos = []
     taille = []
-    for obj in niv.objets:
+    for obj in niv.plateformes:
         if obj.rect.topleft in pos and obj.rect.size in taille:
             obj.supprimer()
             nombre_de_suppression += 1
@@ -40,10 +32,9 @@ for niv in Niveau.liste:
         else:
             pos.append(obj.rect.topleft)
             taille.append(obj.rect.size)
-    Plateforme.liste[niv.numero] = sorted(niv.objets, key=lambda x: (- x.nb_positions, x.pos1[0]))
+    niv.plateformes = sorted(niv.plateformes, key=lambda x: (- x.nb_positions, x.pos1[0]))
 print("\033[32msupression de", nombre_de_suppression, "objets réussi\033[0m")
 
-
-for i in range(1, Niveau.nombre + 1):
-    with open( "objets/plateforme"+str(i)+".json", "w") as f:
-        json.dump([plat.to_dict() for plat in Plateforme.liste[i]], f, indent=4)
+for i in range(Niveau.nombre):
+    with open( "objets/plateforme"+str(i+1)+".json", "w") as f:
+        json.dump([plat.to_dict() for plat in Niveau.liste[i].plateformes], f, indent=4)
